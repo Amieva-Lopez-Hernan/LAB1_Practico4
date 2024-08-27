94525b241c2fd6862ea9509d72872bb36ecd25ad
Amieva-Lopez-Hernan <hernan.amieva.lopez@hotmail.com>
2024-08-27 20:42:24 -0300

    Creacion del proyecto

---
+++ src/laboratorio/trabajopractico4/ejercicio1/LaboratorioTrabajoPractico4Ejercicio1.java
@@ -0,0 +1,12 @@
+package laboratorio.trabajopractico4.ejercicio1;
+
+/**
+ *
+ * @author Hernan
+ */
+public class LaboratorioTrabajoPractico4Ejercicio1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
