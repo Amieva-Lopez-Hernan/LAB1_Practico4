e3c347952c4152fb0e45513bd13cd1f141ba82da
jgouffier <gouffierjulian@gmail.com>
2024-08-29 17:30:42 -0300

    Subo entidades

---
+++ src/laboratorio/trabajopractico4/ejercicio1/Alumno.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio.trabajopractico4.ejercicio1;
+
+/**
+ *
+ * @author gouff
+ */
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
